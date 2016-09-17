0f7ead4c20b97d6f4d4d936b3096498851770ec0
Administrator <Administrator@PC201608311226>
2016-09-17 09:27:25 +0800

    ee

--- src/main/java/com/qkl/tfcc/api/service/acc/api/ComAccMyService.java
+++ src/main/java/com/qkl/tfcc/api/service/acc/api/ComAccMyService.java
@@ -1,34 +1,21 @@
 package com.qkl.tfcc.api.service.acc.api;
 
-import java.math.BigDecimal;
+
 import java.util.List;
 import java.util.Map;
-
 import com.qkl.tfcc.api.entity.Page;
-import com.qkl.tfcc.api.po.acc.ComAccMy;
 import com.qkl.util.help.pager.PageData;
+
+
 
 public interface ComAccMyService {
 
 	
-	
-	 //查询TFCC的余额
-	 
-	public BigDecimal findTB(String userCode);
-	
-	//查询JFFC的余额
-	
-	public BigDecimal findJB(String userCode);
-	
-	//查询推荐会员奖励的TFCC
-	public BigDecimal findReward(String userCode);
-		
-	//查询推荐网点奖励的TFCC
-	public BigDecimal findWReward(String userCode);
-		
-	//查询累奖励的TFCC
-	public BigDecimal findTTReward(String userCode);
-	//查询推荐各级别会员所得奖励列表
+	/**
+	 * 查询推荐会员所得奖励的列表
+	 * @param page
+	 * @return
+	 */
 	
 	public List<PageData> findAll(Page page);
 	
